2a4f433a8fb678f31c40bbe4f159207b36e08a42
asensiodev <asensiodev@gmail.com>
2024-04-23 17:53:16 +0200

    Add DuckCall without inheritance

---
+++ ducks_chapter_one/src/main/kotlin/org/asensiodev/ducks/DuckCall.kt
@@ -0,0 +1,11 @@
+package org.asensiodev.org.asensiodev.ducks
+
+import org.asensiodev.org.asensiodev.behaviour_interfaces.QuackBehaviour
+
+class DuckCall(
+    var quackBehaviour: QuackBehaviour
+) {
+    fun makeQuackSound() {
+        quackBehaviour.
+    }
+}
